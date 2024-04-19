10a6ce706aa171b43e4aa96dc9e953d173d326ea
Clarissa B. Vega <issayvega@gmail.com>
2024-04-19 09:57:49 +0800

    ud

--- delete.php
+++ delete.php
@@ -1,12 +1,9 @@
 <?php
-    $stud_id = $_GET["id"];
+    $student_id = $_GET['id'];
+    include('conn.php');
 
-    $conn = new mysqli($servername, $username, $password, $db_name);
-    if ($conn->connect_errno){
-        die ('Connection Failed'.$conn->error);
-    }
-
-    $sql = "DELETE FROM tbl_student where stud_id='".$stud_id."'";
+    $sql = "DELETE FROM tbl_student where 
+             student_id='".$student_id."'";
     $conn ->query($sql);
 
     header('location:srms.php');
